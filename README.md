d77a0cdff762d290340a70bb2436511f1f938e65
dungtvict <dungtvk48@gmail.com>
2019-11-21 14:33:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-getting-started-99
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-getting-started-99)
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'iPhone 11',
+    price: 399,
+    description: 'A new dual‑camera system captures more of what you see and love. The fastest chip ever in a smartphone and all‑day battery life let you do more and charge less. And the highest‑quality video in a smartphone, so your memories look better than ever'
+  },
+  {
+    name: 'iPhone 11 Pro',
+    price: 599,
+    description: 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro'
+  },
+  {
+    name: 'AirPods',
+    price: 199,
+    description: ''
+  }
+]
